131de14e64971596272cd5aafdf20d8c6d3be67a
Stuart Weir <98584679+Stuart-Weir@users.noreply.github.com>
2022-04-26 19:19:40 +0100

    Update end.js

--- end.js
+++ end.js
@@ -25,5 +25,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/Users/maide/Desktop/WebTechCoursework/Full_to_style/highscores.html');
+    window.location.assign('https://stuart-weir.github.io/Website/highscores.html');
 };

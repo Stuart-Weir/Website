910d0d6467b0cdccd5a98894a7f5e853b9659ad1
Stuart Weir <98584679+Stuart-Weir@users.noreply.github.com>
2022-04-29 08:41:06 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-// code modified from https://www.youtube.com/watch?v=u98ROZjBWy8&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx
-
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
